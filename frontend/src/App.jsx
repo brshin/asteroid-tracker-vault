40d7d58ebf6acba03cec4fc03e63ce206bf92720
Brandon Shin <143565532+brshin@users.noreply.github.com>
2026-05-17 17:58:52 -0700

    Wired up frontend button to POST favorite asteroids to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,23 @@ function App() {
             .catch(err => console.error("Network error:", err));
     }, []);
 
+    const handleSaveAsteroid = (asteroid) => {
+
+        fetch('http://localhost:3000/asteroids/favorites', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: asteroid.name,
+                potentiallyHazardous: asteroid.potentiallyHazardous
+            })
+        })
+        .then(res => res.json())
+        .then(data => console.log("Success! Server says: ", data))
+        .catch(err => console.error("Failed to add:", err));
+    };
+
     return (
         <div>
             <h1>Asteroid Dashboard</h1>
@@ -23,6 +40,10 @@ function App() {
                 <div key={asteroid.name}>
                     <h2>{asteroid.name}</h2>
                     <p>Hazardous: {asteroid.potentiallyHazardous.toString()}</p>
+
+                    <button onClick={() => handleSaveAsteroid(asteroid)}>
+                        Favorite
+                    </button>
                 </div>
             ))}
 
